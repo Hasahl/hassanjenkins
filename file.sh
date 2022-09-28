776866b2bf21f8ad536553ec5a3f0d370b5cf62f
Hasahl <110679440+Hasahl@users.noreply.github.com>
2022-09-28 16:39:28 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,1 +1,8 @@
 echo 'hello world'
+
+sudo docker tag sahal/bitnami:java
+sudo docker tag sahal/bitnami:java
+sudo docker tag bitnami/java sahal/bitnami:java
+sudo docker push sahal/bitnami:java
+RM syntax: sudo docker rm -f localnginx
+sudo docker ps
